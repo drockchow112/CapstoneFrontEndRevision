fe635e0006b6b04b6e9f56c1f43f3d7db1d595c9
Derrick <derrick.chow234@gmail.com>
2020-06-16 16:03:14 -0400

    Added new navbar

---
+++ src/components/views/NewNavBarView.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class NewNavBarView extends Component {
+  render() {
+    return (
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-info">
+        <a class="navbar-brand" href="#">
+          APP_NAME or APP_LOGO
+        </a>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Home{" "}
+              </a>
+            </li>
+            <li class="nav-item">
+              <Link to="/users" className="nav-link">
+                Users
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link to="/signup" className="nav-link">
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+          <form class="form-inline mt-2 mt-md-0">
+            <input
+              class="form-control mr-sm-2"
+              type="text"
+              placeholder="Search"
+              aria-label="Search"
+            ></input>
+            <button
+              class="btn btn-outline-light my-2 my-sm-0 bg-primary"
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default NewNavBarView;
--- src/components/views/index.js
+++ src/components/views/index.js
@@ -1,4 +1,4 @@
 // Barrel file for presentational components, which will be used to compose our smart containers;
-export { default as NavBarView } from "./NavBarView";
-export {default as SignupFormView}from "./SignupFormView";
-export {default as AllUsersView}from "./AllUsersView";
+export { default as NewNavBarView } from "./NewNavBarView";
+export { default as SignupFormView } from "./SignupFormView";
+export { default as AllUsersView } from "./AllUsersView";
