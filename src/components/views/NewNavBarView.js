dd0a8d4da11872ea5fc237819e2415bf5bfd85e8
Lamarr Baker <bakerlamarr@gmail.com>
2020-06-16 23:16:26 -0400

    fixed altered containers

---
+++ src/components/views/NewNavBarView.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NewNavBarView =(props)=> {
+    return (
+      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-info">
+        <a className="navbar-brand" href="#">
+          APP_NAME or APP_LOGO
+        </a>
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to="/home" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/users" className="nav-link">
+                Users
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/signup" className="nav-link">
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+          <form className="form-inline mt-2 mt-md-0" 
+            onSubmit={props.handleSubmit}>
+            <input
+              className="form-control mr-sm-2"
+              type="text"
+              placeholder="Search"
+              aria-label="Search"
+              onChange={props.handleChange}
+            ></input>
+            <button
+              className="btn btn-outline-light my-2 my-sm-0 bg-primary"
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+    );
+}
+
+NewNavBarView.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
+
+export default NewNavBarView;
