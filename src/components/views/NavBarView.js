a7eb43156a1fac081d4ca543de06346b7d955d98
Derrick <derrick.chow234@gmail.com>
2021-02-11 23:47:46 -0500

    Changed navbar styling

--- src/components/views/NavBarView.js
+++ src/components/views/NavBarView.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "../../styles/NavBarView.css";
+import { FaSearch } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
 
 const NavBarView = (props) => {
   let LoginDisplay;
@@ -16,38 +18,21 @@ const NavBarView = (props) => {
   }
 
   return (
-    <nav className="header">
-      <Link to="/home" className="navbar-brand">
-        CHEAPEST PRICE!
-      </Link>
-      <div className="collapse navbar-collapse" id="navbarCollapse">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <Link to="/products" className="nav-link">
-              Products
-            </Link>
-          </li>
-
-          {LoginDisplay}
-        </ul>
-        <form
-          className="form-inline mt-2 mt-md-0"
-          onSubmit={props.handleSubmit}
-        >
-          <input
-            value={props.search}
-            name="search"
-            onChange={props.handleChange}
-            className="form-control mr-sm-2"
-            type="text"
-            placeholder="Search"
-            aria-label="Search"
-          ></input>
-        </form>
-      </div>
-    </nav>
+    <>
+      <nav className="header">
+        <h2>Not Amazon</h2>
+        <div className="search">
+          <input type="search" placeholder="Search" />
+          <IconContext.Provider value={{ marginRight: "20px", color: "blue" }}>
+            <FaSearch />
+          </IconContext.Provider>
+        </div>
+        <button>Log In</button>
+      </nav>
+    </>
   );
 };
+
 NavBarView.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
   handleChange: PropTypes.func.isRequired,
